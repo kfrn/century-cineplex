06d9d2a6e51f4dbd30fff430fae02f9eab2391d1
kfrn <knfrances@gmail.com>
2016-11-15 18:52:00 +1300

    Create FN to build up URL from current date

--- functions/getIMDbIDs.js
+++ functions/getIMDbIDs.js
@@ -31,12 +31,24 @@ function extractIDs(arrayofURLs) {
   return IMDbIDs.map(elem => elem[0])
 }
 
+function buildIMDbURL() {
+  var currentDate = new Date
+  var centuryAgo = currentDate.getFullYear() - 100
+  var month = currentDate.getMonth()+1
+  console.log("month is", month, centuryAgo);
+  var URL = "http://www.imdb.com/search/title?count=500&release_date=" + centuryAgo + "-" + month + "-01," + centuryAgo + "-" + month + "-30" + "&title_type=feature,documentary,short"
+  console.log(URL);
+  return URL
+  // http://www.imdb.com/search/title?count=500&release_date=1916-11-01,1916-11-30&title_type=feature,documentary,short
+}
+
 function getIMDbIDs() {
-  getPage("http://www.imdb.com/search/title?release_date=1895-01-01,1895-06-30&sort=year,asc&sound_mixes=silent")
+  getPage(buildIMDbURL())
     .then (function(result) {
       var IMDbURLs = extractURLs(result)
       var IMDbIDs = extractIDs(IMDbURLs)
-      console.log("The IMDbIDs are:", IMDbIDs)
+      // console.log("The IMDbIDs are", IMDbIDs);
+      console.log("The # of IMDbIDs is:", IMDbIDs.length)
     })
     .catch (function(error) {
       console.log(error);
