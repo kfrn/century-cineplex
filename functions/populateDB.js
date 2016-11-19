5e9e40ba59b65074a75d52b24621c21daee88062
Katherine Nagels <knfrances@gmail.com>
2016-11-19 21:01:17 +1300

    Change log message

--- functions/populateDB.js
+++ functions/populateDB.js
@@ -4,12 +4,12 @@ var getFilmData = require('./getFilmData')
 module.exports = populateDB
 
 function populateDB() {
-  getIMDbIDs.getPage(getIMDbIDs.buildIMDbURL()) // 
+  getIMDbIDs.getPage(getIMDbIDs.buildIMDbURL()) //
     .then (function(result) {
       var IMDbURLs = getIMDbIDs.extractURLs(result)
       var IMDbIDs = getIMDbIDs.extractIDs(IMDbURLs)
       // console.log("The IMDbIDs are", IMDbIDs);
-      console.log("The # of IMDbIDs is:", IMDbIDs.length)
+      console.log(`This month, there were ${IMDbIDs.length} films`)
       IMDbIDs.forEach(getFilmData)
     })
     .catch (function(error) {
