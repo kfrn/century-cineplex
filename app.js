9f0ffc59df0be3984a3eeef5a01ecfc59bc6f100
kfrn <knfrances@gmail.com>
2016-11-20 18:37:20 +1300

    Add handlebars helper

--- app.js
+++ app.js
@@ -4,11 +4,12 @@ var favicon = require('serve-favicon')
 var logger = require('morgan')
 var cookieParser = require('cookie-parser')
 var bodyParser = require('body-parser')
+var hbs = require('hbs')
 
 var index = require('./routes/index')
-var users = require('./routes/users')
+// var users = require('./routes/users')
 
-var clearDB = require('./functions/dbBasics').clearDB
+// var clearDB = require('./functions/dbBasics').clearDB
 var populateDB = require('./functions/populateDB')
 
 var app = express()
@@ -27,7 +28,7 @@ app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', index)
-app.use('/users', users)
+// app.use('/users', users)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -45,6 +46,14 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500)
   res.render('error')
+})
+
+hbs.registerHelper('selected', function(option, value){
+  if (option === value){
+    return 'selected'
+  } else {
+    return ''
+  }
 })
 
 // Populate database
