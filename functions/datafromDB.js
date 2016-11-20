c2357aeefed097f9b2d87c8df68632e54d1e5d20
kfrn <knfrances@gmail.com>
2016-11-20 19:11:45 +1300

    Write (and call) FN to get the genres

--- functions/datafromDB.js
+++ functions/datafromDB.js
@@ -4,7 +4,8 @@ var knex = Knex(config)
 
 module.exports = {
   getFilms,
-  getCountries
+  getCountries,
+  getGenres
 }
 
 function getFilms() {
@@ -18,6 +19,21 @@ function getCountries() {
           .distinct('countries')
           // .select()
 }
+
+function getGenres() {
+  return knex('films')
+          .distinct('genres')
+}
+
+// getGenres()
+//   .then(function(req) {
+//     var genreList = req.map((elem) => elem.genres).join(', ').split(", ").filter((item, idx, self) => idx == self.indexOf(item)).sort()
+//     console.log(genreList)
+//
+//   })
+//   .catch(function(error) {
+//     console.log(error)
+//   })
 
 
 /* Raw SQL */
