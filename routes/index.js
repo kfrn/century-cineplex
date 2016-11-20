f73176329a5ca8528723866e7852769b99b32e5d
kfrn <knfrances@gmail.com>
2016-11-20 14:38:43 +1300

    Update index route & view

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+var currentDate = new Date
+var centuryAgo = currentDate.getFullYear() - 100
+var locale = 'en-us'
+var month = currentDate.toLocaleString(locale, {month: 'long'})
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Century Cineplex' });
+  res.render('index', { title: 'Century Cineplex', year: centuryAgo, month: month });
 });
 
 module.exports = router;
