f7247eaa8256dedb3d27b3c59ddccd821debf723
kfrn <knfrances@gmail.com>
2016-11-24 19:57:52 +1300

    Add case to search only by genre

--- functions/datafromDB.js
+++ functions/datafromDB.js
@@ -27,30 +27,31 @@ function getGenres() {
 }
 
 function getSearchResults(country, genre, plot) {
-  if (country != 'any' && genre != 'any' && plot == 'on') { // HAS country, plot, genre
+  // genre = /genre/
+  if (country != 'any' && genre != 'any' && plot == 'on') { // HAS country, HAS plot, HAS genre
     return knex('films')
     .where('countries', '=', country)
     .andWhere('genres', 'like', genre)
     .andWhere('plot', '<>', '')
-  } else if (country != 'any' && genre != 'any' && plot != 'on') { // HAS country, genre; NO plot
-    return knex('films')
-    .where('countries', '=', country)
-    .andWhere('genres', 'like', genre)
-    // .andWhere('plot', '=', '')
-  } else if (country != 'any' && genre == 'any' && plot != 'on') { // HAS country; NO genre or plot
-    return knex('films')
-    .where('countries', '=', country)
-    // .andWhere('plot', '=', '')
-  } else if (country == 'any' && genre != 'any' && plot == 'on') { // NO country; HAS genre, plot
-    return knex('films')
-    .andWhere('genres', 'like', genre)
-    .andWhere('plot', '<>', '')
-  } else if (country == 'any' && genre == 'any' && plot == 'on') { // NO country, genre; HAS plot
-    return knex('films')
-    .andWhere('plot', '<>', '')
-  } else if (country == 'any' && genre == 'any' && plot != 'on') { // NO country; genre, plot
-    return knex('films')
-    // .andWhere('plot', '=', '')
+  } else if (country != 'any' && genre != 'any' && plot != 'on') { // HAS country, HAS genre; NO plot
+      return knex('films')
+      .where('countries', '=', country)
+      .andWhere('genres', 'like', genre)
+  } else if (country != 'any' && genre == 'any' && plot != 'on') { // HAS country; NO genre, NO plot
+      return knex('films')
+      .where('countries', '=', country)
+  } else if (country == 'any' && genre != 'any' && plot == 'on') { // NO country; HAS genre, HAS plot
+      return knex('films')
+      .andWhere('genres', 'like', genre)
+      .andWhere('plot', '<>', '')
+  } else if (country == 'any' && genre == 'any' && plot == 'on') { // NO country, NO genre; HAS plot
+      return knex('films')
+      .andWhere('plot', '<>', '')
+  } else if (country == 'any' && genre != 'any' && plot != 'on') { // NO country, HAS genre NO plot
+      return knex('films')
+      .andWhere('genres', 'like', genre)
+  } else if (country == 'any' && genre == 'any' && plot != 'on') { // NO country; NO genre, NO plot
+      return knex('films')
   }
 }
 
