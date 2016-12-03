60aef76c3b8ee3fdce6fd1698c514c1b65faba5a
kfrn <knfrances@gmail.com>
2016-12-03 16:29:50 +1300

    Update DB queries, adding wildcard char to be able to get country if the film has more than one

--- functions/datafromDB.js
+++ functions/datafromDB.js
@@ -29,19 +29,19 @@ function getGenres() {
 function getSearchResults(country, genre, plot) {
   if (country != 'any' && genre != 'any' && plot == 'on') { // HAS country, HAS plot, HAS genre
     return knex('films')
-    .where('countries', 'like', country)
-    .andWhere('genres', 'like', '%' +  genre + '%')
+    .where('countries', 'like', '%' + country + '%')
+    .andWhere('genres', 'like', '%' + genre + '%')
     .andWhere('plot', '<>', '')
   } else if (country != 'any' && genre != 'any' && plot != 'on') { // HAS country, HAS genre; NO plot
       return knex('films')
-      .where('countries', 'like', country)
-      .andWhere('genres', 'like', '%' +  genre + '%')
+      .where('countries', 'like', '%' + country + '%')
+      .andWhere('genres', 'like', '%' + genre + '%')
   } else if (country != 'any' && genre == 'any' && plot != 'on') { // HAS country, NO genre; NO plot
       return knex('films')
-      .where('countries', 'like', country)
+      .where('countries', 'like', '%' + country + '%')
   } else if (country != 'any' && genre == 'any' && plot == 'on') { // HAS country; NO genre, HAS plot
       return knex('films')
-      .where('countries', 'like', country)
+      .where('countries', 'like', '%' + country + '%')
       .andWhere('plot', '<>', '')
   } else if (country == 'any' && genre != 'any' && plot == 'on') { // NO country; HAS genre, HAS plot
       return knex('films')
