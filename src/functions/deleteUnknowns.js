2e48391e290ea5002a15e644bca0e045f1dfe1b3
Katherine Nagels <knfrances@gmail.com>
2017-02-01 10:15:54 +1300

    Refolder

--- src/functions/deleteUnknowns.js
+++
@@ -1,7 +0,0 @@
-module.exports = deleteUnknowns
-
-function deleteUnknowns (filmObj) {
-  if (filmObj.plot === 'unknown') delete filmObj.plot
-  if (filmObj.runtime === 'unknown') delete filmObj.runtime
-  return filmObj
-}
